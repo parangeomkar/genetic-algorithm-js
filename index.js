2f365181d24c31d3c30a7740e4eaab53cad236a7
parangeomkar <zero.omkar@gmail.com>
2022-11-06 03:13:15 +0530

    Fixes GA implementation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-class GA {
+export default class GA {
     constructor(config) {
         this.numParams = config.numParams;
         this.lowerLim = config.lowerLim;
@@ -44,41 +44,56 @@ class GA {
         }).reverse();
 
         for (let i = 0; i < this.popSize * 2; i++) {
-            if (performance[i] == undefined) break;
+            if (performance[i] == undefined || isNaN(performance[i])) {
+                break;
+            };
 
             let sortedPerfIdx = this.performance.indexOf(performance[i]);
             population[i] = this.population[sortedPerfIdx];
         }
 
-        this.population = population;
-        this.performance = performance;
+        this.population = [...population];
+        this.performance = [...performance];
     }
 
-    mutate(child1, child2) {
-        let mutatedChild1 = [...child1],
-            mutatedChild2 = [...child2];
+    mutate(child) {
+        let mutatedChild = [...child];
 
-        for (let i = 0; i < this.numParams; i++) {
-            let binary1 = (mutatedChild1[i] >>> 0).toString(2);
-            let binary2 = (mutatedChild2[i] >>> 0).toString(2);
+        for (let j = 0; j < this.numParams; j++) {
 
-            let idx1 = 1 + Math.floor(Math.random() * (binary1.length - 2));
-            let idx2 = 1 + Math.floor(Math.random() * (binary1.length - 2));
+            let limitDiff = (this.upperLim[j] - this.lowerLim[j]);
 
-            let str1 = !parseInt(binary1[idx1]) ? 1 : 0;
-            let str2 = !parseInt(binary2[idx2]) ? 1 : 0;
+            mutatedChild[j] += Math.floor(0.1 * ((Math.random() - 0.5) * 2 * limitDiff));
 
-            binary1 = binary1.substring(0, idx1) + str1 + binary1.substring(idx1 + 1);
-            binary2 = binary2.substring(0, idx2) + str2 + binary2.substring(idx2 + 1);
+            mutatedChild[j] = Math.min(this.upperLim[j], mutatedChild[j]);
+            mutatedChild[j] = Math.max(this.lowerLim[j], mutatedChild[j]);
 
-            mutatedChild1[i] = Math.min(1000, parseInt(binary1, 2));
-            mutatedChild2[i] = Math.min(1000, parseInt(binary2, 2));
-
-            mutatedChild1[i] = Math.max(-1000, mutatedChild1[i]);
-            mutatedChild2[i] = Math.max(-1000, mutatedChild2[i]);
         }
 
-        return [mutatedChild1, mutatedChild2];
+
+        return mutatedChild;
+
+        // for (let i = 0; i < this.numParams; i++) {
+        //     let binary1 = (mutatedChild1[i] >>> 0).toString(2);
+        //     let binary2 = (mutatedChild2[i] >>> 0).toString(2);
+
+        //     let idx1 = Math.floor(Math.random() * binary1.length);
+        //     let idx2 = Math.floor(Math.random() * binary2.length);
+
+        //     let str1 = !parseInt(binary1[idx1]) ? 1 : 0;
+        //     let str2 = !parseInt(binary2[idx2]) ? 1 : 0;
+
+        //     binary1 = binary1.substring(0, idx1) + str1 + binary1.substring(idx1 + 1);
+        //     binary2 = binary2.substring(0, idx2) + str2 + binary2.substring(idx2 + 1);
+
+        //     mutatedChild1[i] = Math.min(this.upperLim[i], parseInt(binary1, 2));
+        //     mutatedChild2[i] = Math.min(this.upperLim[i], parseInt(binary2, 2));
+
+        //     mutatedChild1[i] = Math.max(this.lowerLim[i], mutatedChild1[i]);
+        //     mutatedChild2[i] = Math.max(this.lowerLim[i], mutatedChild2[i]);
+        // }
+
+        // return [mutatedChild1, mutatedChild2];
     }
 
     crossover(p1, p2) {
@@ -91,15 +106,32 @@ class GA {
             split = Math.floor(Math.random() * this.numParams);
         }
 
-        let child1 = [...parent1];
-        let child2 = [...parent2];
-
-        for (let i = split; i < this.numParams; i++) {
-            child1[i] = parent2[i];
-            child2[i] = parent1[i];
-        }
+        let child1 = [...[...parent1].slice(0, split), ...[...parent2].slice(split, this.numParams)],
+            child2 = [...[...parent2].slice(0, split), ...[...parent1].slice(split, this.numParams)];
 
         return [child1, child2];
+
+        // let crossoverChild1 = [...this.population[p1]],
+        //     crossoverChild2 = [...this.population[p2]];
+
+        // for (let i = 0; i < this.numParams; i++) {
+        //     let binary1 = (crossoverChild1[i]).toString(2);
+        //     let binary2 = (crossoverChild2[i]).toString(2);
+
+        //     let idx1 = Math.floor(Math.random() * (binary1.length - 2));
+        //     let idx2 = Math.floor(Math.random() * (binary2.length - 2));
+
+        //     let binary11 = binary1.substring(0, idx1) + binary2.substring(idx2 + 1);
+        //     let binary22 = binary2.substring(0, idx2) + binary1.substring(idx1 + 1);
+
+        //     crossoverChild1[i] = Math.min(this.upperLim[i], parseInt(binary11, 2));
+        //     crossoverChild2[i] = Math.min(this.upperLim[i], parseInt(binary22, 2));
+
+        //     crossoverChild1[i] = Math.max(this.lowerLim[i], crossoverChild1[i]);
+        //     crossoverChild2[i] = Math.max(this.lowerLim[i], crossoverChild2[i]);
+        // }
+
+        // return [crossoverChild1, crossoverChild2];
     }
 
     optimize() {
@@ -111,25 +143,27 @@ class GA {
         for (let i = 0; i < this.maxGen; i++) {
 
             while (currentPopSize < this.popSize * 2) {
-                let idxParent1 = -1,
-                    idxParent2 = -1;
+                let idxParent1 = 0,
+                    idxParent2 = 0;
 
                 while (idxParent1 == idxParent2) {
                     idxParent1 = Math.floor(Math.random() * this.popSize);
-                    idxParent2 = Math.floor(Math.random() * this.popSize);
+                    idxParent1 = Math.floor(Math.random() * this.popSize);
                 }
 
                 [child1, child2] = this.crossover(idxParent1, idxParent2);
 
+
                 if (Math.random() < this.mutationRate) {
-                    [child1, child2] = this.mutate(child1, child2);
+                    child1 = this.mutate(child1);
+                    child2 = this.mutate(child2);
                 }
 
                 this.performance[currentPopSize] = this.evaluate(child1);
                 this.performance[currentPopSize + 1] = this.evaluate(child2);
 
-                this.population[currentPopSize] = child1;
-                this.population[currentPopSize + 1] = child2;
+                this.population[currentPopSize] = [...child1];
+                this.population[currentPopSize + 1] = [...child2];
 
                 currentPopSize += 2;
             }
@@ -138,30 +172,8 @@ class GA {
 
             currentPopSize = this.popSize;
         }
-        console.log(this.performance[0]);
+
+        console.log(this.performance[0], this.performance[this.popSize]);
         console.log(this.population[0]);
     }
 }
-
-const costFunction = (chromosome) => {
-    let sum = 0;
-    for (let x of chromosome) {
-        if (x == undefined) break;
-        sum += x;
-    }
-    return (-sum);
-}
-
-let numParams = 10;
-let config = {
-    numParams: numParams,
-    mutationRate: 0.2,
-    crossoverRate: 0.6,
-    costFunction,
-    lowerLim: new Array(numParams).fill(0),
-    upperLim: new Array(numParams).fill(1000),
-    popSize: 100,
-    maxGen: 300
-}
-let ga = new GA(config);
-ga.optimize();
